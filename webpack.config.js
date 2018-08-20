b849696b6274cb835248478bc4452a56e34f310b
Brendan Morrell <brendanmorrell@gmail.com>
2018-08-20 15:34:47 -0700

    getting basic structure set up

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,37 @@
-module.exports = {};
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'public', 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        exclude: /node_modules/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public', 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Affogato',
+      template: path.join(__dirname, 'assets', 'template.html'),
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'bundle.css',
+    }),
+  ],
+};
